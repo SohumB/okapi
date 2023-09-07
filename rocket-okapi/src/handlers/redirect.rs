ded1ba41fe87feb3d3754cc081d601c4732059d5
Sohum Banerjea <sohumb@gmail.com>
2023-09-08 07:51:03 +1000

    update to match rocket main branch changes

--- rocket-okapi/src/handlers/redirect.rs
+++ rocket-okapi/src/handlers/redirect.rs
@@ -27,7 +27,7 @@ impl RedirectHandler {
 #[rocket::async_trait]
 impl Handler for RedirectHandler {
     async fn handle<'r>(&self, req: &'r Request<'_>, _: Data<'r>) -> Outcome<'r> {
-        let path = req.route().unwrap().uri.base().trim_end_matches('/');
-        Outcome::from(req, Redirect::to(format!("{}/{}", path, self.dest)))
+        let path = req.route().unwrap().uri.base().to_string();
+        Outcome::from(req, Redirect::to(format!("{}/{}", path.trim_end_matches('/'), self.dest)))
     }
 }
